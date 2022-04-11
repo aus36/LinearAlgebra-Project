c8a2564f3995b9ca16cccb23476a91c55d58aea6
aus36 <austin.hale36@gmail.com>
2022-04-11 16:56:38 -0500

    Initial Commit

---
+++ mainFile.py
@@ -0,0 +1,23 @@
+#Authors: Austin Hale, Zenon Nowakowki
+#Linear Algebra Final Project
+#Topic 1: Perform transformations on vectors using numpy and matplotlib in Python
+
+#imports
+from math import cos, sin
+import numpy as np
+import matplotlib.pyplot as plt
+
+#test plotting
+x = np.linspace(0, 2 * np.pi, 200)
+y = np.sin(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+plt.show()
+
+#rotation of vectors
+theta = np.deg2rad(30)
+rotation = np.array([cos(theta), -sin(theta)],[sin(theta), cos(theta)])
+v1 = np.array([1, 2])
+
+#scaling of vectors using scalar values
