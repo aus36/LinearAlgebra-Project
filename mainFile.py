904852d22c4231c59f0f64a40738230d949ceea3
aus36 <austin.hale36@gmail.com>
2022-04-15 15:26:30 -0500

    Removed old transformation code

--- mainFile.py
+++ mainFile.py
@@ -15,9 +15,4 @@ fig, ax = plt.subplots()
 ax.plot(x, y)
 plt.show()
 
-#rotation of vectors
-theta = np.deg2rad(30)
-rotation = np.array([cos(theta), -sin(theta)],[sin(theta), cos(theta)])
-v1 = np.array([1, 2])
-
 #scaling of vectors using scalar values
